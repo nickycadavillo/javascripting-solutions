81f5795a6bb4ac00f24e8be5e23a6cd3074512a8
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-29 23:24:04 -0500

    passed scope

--- javascripting/scope.js
+++ javascripting/scope.js
@@ -5,6 +5,7 @@ var a = 1, b = 2, c = 3;
 
          (function secondFunction(){
              var b = 8;
+             console.log("a: "+a+", b: "+b+", c: "+c);
 
              (function thirdFunction(){
                  var a = 7, c = 9;
@@ -17,6 +18,5 @@ var a = 1, b = 2, c = 3;
          })();
      })();
 
-console.log("a: "+a+", b: "+b+", c: "+c);
 
 //output should be a: 1, b: 8, c: 6
